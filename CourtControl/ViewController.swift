9dff6803fa73145fa79f34ac7ca4b396ca36aa28
Eugene Lobyr <elobir@gmail.com>
2020-04-29 00:08:54 +0300

    Initial project

---
+++ CourtControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourtControl
+//
+//  Created by e_oneg1n on 28.04.2020.
+//  Copyright © 2020 Homecoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '12.0'
+use_frameworks!
+
+target 'CourtControl' do
+    pod 'Alamofire'
+end
